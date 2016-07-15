11cc636f880b86f9cdf899f801588dc31e641412
WofKybeidos <wof@kybeidos.de>
2016-07-15 17:41:19 +0200

    Email Change funktioniert
    Fehler entdeckt, Nach Email change ist die Session eigentlich nicht mehr
    zu gebrauchen da kein Update von der Session vorliegt
    Optionen:
    - Neue Session starten (d.h. den nutzer ausloggen)
    - Session Updaten ( keine ahnung wie)

--- Verlinkungen/Benutzerverwaltung/Change_Mail.php
+++ Verlinkungen/Benutzerverwaltung/Change_Mail.php
@@ -1,14 +1,14 @@
 <?php 
-session_start();
+
 $pdo = new PDO('mysql:host=localhost;dbname=user', 'root', '');
 $showFormular = true; 
 
 if(isset($_GET['changeMail'])) {
 	$error = false;
 	$password = $_POST['password'];
-	$email = $_SESSION['email'];
 	$emailNew = $_POST['emailNew'];
 	$emailNew2 = $_POST['emailNew2'];
+	$email = $_SESSION['email'];
 	
 
 	if($emailNew != $emailNew2) {
@@ -18,9 +18,9 @@ if(isset($_GET['changeMail'])) {
 	
 	if(!$error) {
 		$stmt = $pdo->prepare("SELECT * FROM user.users WHERE email = :emailNew");
-		$result = $stmt->execute(array('emailNew' => $email));
+		$result = $stmt->execute(array('emailNew' => $emailNew));
 		$user = $stmt->fetch();
-			
+		
 		if($user !== false) {
 			echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
 			$error = true;
@@ -28,9 +28,13 @@ if(isset($_GET['changeMail'])) {
 	}
 		
 	if (!$error){	
+		$stmt = $pdo->prepare("SELECT * FROM user.users WHERE email = :email");
+		$result = $stmt->execute(array('email' => $email));
+		$user = $stmt->fetch();
+		
 		if (hash('sha256',$password) == $user['password']) {
 			$stmt = $pdo->prepare("UPDATE user.users SET email = :emailNew WHERE email = :email");
-			$result = $stmt->execute(array('email' => $emailNew));
+			$result = $stmt->execute(array('emailNew' => $emailNew, 'email' => $email));
 			
 			if($result) {		
 				echo 'E-Mail erfolgreich geändert';
