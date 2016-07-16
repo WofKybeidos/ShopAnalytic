e3c924d1d07d720e4164f91e87701e6b3169af25
WofKybeidos <wof@kybeidos.de>
2016-07-16 14:37:17 +0200

    Preperation for Charts

--- www/includes/charts.php
+++ www/includes/charts.php
@@ -1,6 +1,26 @@
 <?php
 	include( __DIR__ . '\config.php');
+	try{
+		$userID = $_SESSION['userid'];
+		$result = $pdo->query('SELECT Category, Price FROM user.transaction WHERE userID = :ID_USERS');
+		
+		$rows = array();
+		$table = array();
+		$table['cols'] = array(
+			array('label' => 'Kategorie', 'type' => 'string'),
+			array('label' => 'Ausgaben'. 'type' => 'number'));
+			
+		foreach($result){
+			$temp = array();
+			$temp[] = array('v' => (string) $result['Category']);
+			
+			$temp[] = array('v' => (int) $result['Price']);
+		}
+		
+		$table['rows'] = $rows;
+		$jsonTable = json_encode($table);
 	
-	$userID = $_SESSION['userid'];
-	$result = $pdo->query('SELECT Category, Price FROM Transaction WHERE 
+	} catch(PDOException $e){
+		echo 'ERROR: ' . $e ->getMessage();
+	}
 ?>
