8702b3919aa4f2d957d5a73287f56f3596cc924e
JaschaG <jaschagrewe@yahoo.de>
2016-07-18 13:02:01 +0200

    Kommentierung der Start.php

--- www/Start.php
+++ www/Start.php
@@ -1,9 +1,10 @@
+<!-- Zur Modularisierung haben wir ein externes Login.php eingebunden-->
 <?php include(__DIR__ . '\includes\login.php'); ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<!-- für google Optimierte Such kriterien beachten -->
+		<!-- Für Google optimierte Suchkriterien mit Seitenrelevanten Informationen und Beschreibung -->
 		<title>Login Shopalytics</title>
 		<meta name="description" content="Login to Shopalytics Webapplication"/>
 		<meta name="language" content="de"/>
@@ -14,11 +15,13 @@
 	</head>
 	
 	<header>
+	<!-- Einbindung des Logos im Header, damit es oben auf der Seite ausgerichtet werden kann -->
 		<img src="../graphic/logos/Shopanalytics.png">
 	</header>
 	
 	<body>
-			
+		<!-- Definierung des Loginprozesses im Fieldset mit den benötigten Feldern (Mit dem Typ email, passwort und button) und Übergabe der Werte an 
+		das dementsprechende PHP und die dazugehörigen Links -->	
 		<form method="post" name="login" action="?login=1">
 			<fieldset>
 				<input type="email" name="email" id="email" placeholder="E-Mail-Adresse" autocomplete="on" autocorrect="off" required>  
@@ -31,9 +34,12 @@
 				<input type="submit" class="Button" title="Einloggen" value="Einloggen"/>
 			</fieldset>
 		</form>
+		<!-- Die Funktion hinter dem Passwort vergessen konnte aus zeitlichen Gründen leider nicht mehr implementiert werden, der Vollständigkeitshalber  
+		haben wir es aber drinnen gelassen -->
 		<a id = "PwVergessen" href="">Passwort vergessen?</a>
 	</body>
-	
+	<!-- Definierung des Footers mit dem Links zum Impressum, Datenschutzbestimmungen und Kontaktfomulat. Hier eingebunden, damit man direkt von 
+	der Loginseite darauf zugreifen kann -->
 	<footer>
 		<a href="../Verlinkungen/Impressum.html">Impressum</a> | 
 		<a href="../Verlinkungen/Datenschutz.html">Datenschutzbestimmungen</a> | 
